9e8dd4ff710e23e09e28d65ed69a3f63aef5da22
AliceMeneses <alicesilvademeneses@gmail.com>
2021-08-25 13:45:55 -0300

    Adicionar navegação para o cadastro de leilão e verificar se o leilão foi realmente cadastrado buscando ele na lista de leilões

--- src/test/java/br/com/alura/leilao/LeiloesPage.java
+++ src/test/java/br/com/alura/leilao/LeiloesPage.java
@@ -2,7 +2,7 @@ package br.com.alura.leilao;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 import br.com.alura.PageObject;
 import br.com.alura.leilao.login.LoginPage;
@@ -14,6 +14,10 @@ public class LeiloesPage extends PageObject{
 	public LeiloesPage() {
 		super();
 		getBrowser().navigate().to(URL_LEILOES);
+	}
+
+	public LeiloesPage(WebDriver browser) {
+		super(browser);
 	}
 
 	public LoginPage navegaParaPaginaDeLogin() {
@@ -30,6 +34,28 @@ public class LeiloesPage extends PageObject{
 		} else {
 			return new LoginPage(getBrowser());
 		}
+	}
+
+	public CadastroLeilaoPage navegarParaPaginaDeCadastroDeLeiLao() {
+		super.getBrowser().findElement(By.id("novo_leilao_link")).click();
+		return new CadastroLeilaoPage(super.getBrowser());
+		
+	}
+
+	public boolean contemTexto(String texto) {
+		
+		return super.getBrowser().getPageSource().contains(texto);	
+	}
+
+	public boolean isLeilaoCadastrado(String nome, String valorInicial, String dataDeAbertura) {
+		WebElement ultimaLinhaDaTabela = super.getBrowser().findElement(By.cssSelector("table tbody tr:last-child"));
+		
+		WebElement colunaNome = ultimaLinhaDaTabela.findElement(By.cssSelector("td:nth-child(1)"));
+		WebElement colunaDataDeAbertura = ultimaLinhaDaTabela.findElement(By.cssSelector("td:nth-child(2)"));
+		WebElement colunaValorInicial = ultimaLinhaDaTabela.findElement(By.cssSelector("td:nth-child(3)"));
+
+		return colunaNome.getText().equals(nome) && colunaDataDeAbertura.getText().equals(dataDeAbertura) 
+				&& colunaValorInicial.getText().equals(valorInicial);
 	}
 
 	
